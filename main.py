685395f3b7defd7687f06b2f41f040b3fffdeb6a
Yingqiang Liu <53621227+Remilistrasza@users.noreply.github.com>
2023-12-20 23:48:30 -0700

    Create main.py

---
+++ main.py
@@ -0,0 +1,16 @@
+from sqlalchemy import create_engine
+from sqlalchemy.sql import text
+import os
+
+# Fetch database connection details from environment variables
+db_path = os.getenv('SQLITE_DB_PATH')
+
+engine = create_engine(f'sqlite:///{db_path}')
+
+def run_query(sql_text):
+    output = ""
+    with engine.connect() as connection:
+        result = connection.execute(text(sql_text))
+    for row in result:
+        output += str(row)
+    return output
